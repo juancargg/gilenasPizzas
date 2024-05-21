2f2d526e0bc0d70c397d918414ceb7a388d733b5
juancargg <juancarggIT@gmail.com>
2024-05-21 21:31:55 +0200

    Inicio del proyecto

---
+++ app/products/new/page.tsx
@@ -0,0 +1,8 @@
+export default function CreateProductPage() {
+
+    console.log('Desde el servidor')
+
+  return (
+    <div>CreateProductPage</div>
+  )
+}
---
+++ prisma/data/categories.ts
@@ -0,0 +1,26 @@
+export const categories = [
+    {
+        slug: "cafe",
+        name: "Café"
+      },
+      {
+        slug: "hamburguesa",
+        name: "Hamburguesas"
+      },
+      {
+        slug: "pizza",
+        name: "Pizzas"
+      },
+      {
+        slug: "bolleria",
+        name: "Bollería"
+      },
+      {
+        slug: "pastel",
+        name: "Pasteles"
+      },
+      {
+        slug: "galletas",
+        name: "Galletas"
+      }
+]
