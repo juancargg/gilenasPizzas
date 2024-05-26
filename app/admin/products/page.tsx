fc314571313a8a06e220dbbb5583cae5df596260
juancargg <juancarggIT@gmail.com>
2024-05-26 22:20:45 +0200

    pantalla admin productos empezada

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,10 +1,33 @@
+import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function ProductsPage() {
+async function getProducts() {
+  const products = await prisma.product.findMany({
+    include: {
+      category: true
+    }
+  })
+
+  return products
+  
+}
+
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
+
+export default async function ProductsPage() {
+
+  const products = await getProducts()
 
   return (
     <>
       <Heading>Administrar Productos</Heading>
+      
+      <ProductTable 
+        products={products}
+      />
+
+
     </>
   )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -7,6 +7,6 @@ export type OrderItem = Pick<Product, 'id' | 'name' | 'price'> & {
 
 export type OrderWithProducts = Order & {
     orderProducts: (OrderProducts & {
-        products: Product
+        product: Product
     })[]
 }
