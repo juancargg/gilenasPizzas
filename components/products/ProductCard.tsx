980910ae32cfe821e3335b414a073e778820969a
juancargg <juancarggIT@gmail.com>
2024-05-26 12:34:18 +0200

    añadir productos al carrito

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { formatMoney } from "@/src/utils"
 import { Product } from "@prisma/client"
 import Image from "next/image"
+import AddProductButton from "./AddProductButton"
 
 
 
@@ -22,13 +23,12 @@ export default function ProductCard({product}: ProductCardProps) {
 
         <div className="p-5">
            <h3 className="text-2xl font-bold">{product.name}</h3>
-           <p className="mt-5 font-black text-4xl text-amber-500">{formatMoney(product.price)}</p>
+           <p className="mt-5 font-black text-4xl text-amber-500">
+            {formatMoney(product.price)}</p>
 
-            <button 
-                type="button" 
-                className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 
-                uppercase font-bold cursor-pointer"
-                >Añadir</button>
+          <AddProductButton />
+          
+            
         </div>
     </div>
   )
