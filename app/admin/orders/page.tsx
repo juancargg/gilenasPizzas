3007cb205ba91246dd0a1e1373fd2e04ad2876fc
juancargg <juancarggIT@gmail.com>
2024-05-28 20:06:01 +0200

    Montada api para mandar info de las comandas

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,34 +1,26 @@
+"use client"
+import useSWR from "swr";
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
+import { OrderWithProducts } from "@/src/types";
 
-async function getPendingOrders() {
-    const orders=await prisma.order.findMany({
-      where: {
-        status:false
-      }, 
-      include: {
-        orderProducts: {
-          include: {
-            product: true
-          }
-        }
-      }
-    })
+export default function OrdersPAge() {
+  const url = '/admin/orders/api'
+  const fetcher = () => fetch(url).then(res=> res.json()).then(data => data)
+  const { data, error, isLoading}=useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 600000,
+    revalidateOnFocus: false
+  })
 
-    return orders
-}
-
-export default async function OrdersPAge() {
-  const orders = await getPendingOrders()
-
-  return (
+  if(isLoading) return 'Cargando...'
+ 
+  if(data) return (
     <>
       <Heading>Administrar Comandas</Heading>
 
-      {orders.length ? (
+      {data.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map(order => (
+          {data.map(order => (
             <OrderCard 
               key={order.id}
               order={order}
